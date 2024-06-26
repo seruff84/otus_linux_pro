01d140b90bd4d72ff602578683334150ef24d012
drobot <drobot@avselectro.ru>
2024-06-26 16:20:28 +0300

    26

---
+++ 26/ansible/files/project/docker/flask/app/app.py
@@ -0,0 +1,18 @@
+import socket
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    html = "<h3>Hello Fullstack!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" 
+
+    return html.format(hostname=socket.gethostname())
+@app.route("/flask")
+def flask():
+    html = "<h3>Hello Fullstack!!!!!!!!!!!!!!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" 
+
+    return html.format(hostname=socket.gethostname())
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
